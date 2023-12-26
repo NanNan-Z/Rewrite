5a9bb4913f0068fbcfc0657ec922850e8a63257c
NanNan-Z <64877817+NanNan-Z@users.noreply.github.com>
2023-12-26 18:47:14 +0800

    Create pornhub.js

---
+++ pornhub.js
@@ -0,0 +1,15 @@
+[rewrite_local]
+
+^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
+^https?:/\/(cn|www)\.pornhub\.com\/($|(view|video).*$) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/pornhub.js
+
+[mitm] 
+
+hostname = *.pornhub.com
+
+***********************************/
+
+
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/pornhub.css" type="text/css">');
+$done({ body });
