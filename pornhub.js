a820d773bb85fe9833c293b468810c5d3e7a6db8
NanNan-Z <64877817+NanNan-Z@users.noreply.github.com>
2023-12-26 19:32:33 +0800

    Delete pornhub.js

--- pornhub.js
+++
@@ -1,5 +0,0 @@
-hostname = *.pornhub.com
-
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/NanNan-Z/Rewrite/main/pornhub.css" type="text/css">');
-$done({ body });
