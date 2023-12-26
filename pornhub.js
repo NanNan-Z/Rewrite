48e6ab435190e1629d2563c1f9c3165844470524
NanNan-Z <64877817+NanNan-Z@users.noreply.github.com>
2023-12-26 18:59:51 +0800

    Update pornhub.js

--- pornhub.js
+++ pornhub.js
@@ -5,5 +5,5 @@ hostname = *.pornhub.com
 
 
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/pornhub.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/NanNan-Z/Rewrite/main/pornhub.css" type="text/css">');
 $done({ body });
