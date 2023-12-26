404d0b3c6837bb4dbf958d76d4f8f6f0363cb074
NanNan-Z <64877817+NanNan-Z@users.noreply.github.com>
2023-12-26 19:31:32 +0800

    Update pornhub.js

--- pornhub.js
+++ pornhub.js
@@ -1,8 +1,4 @@
 hostname = *.pornhub.com
-    
-^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
-^https?:/\/(cn|www)\.pornhub\.com\/($|(view|video).*$) url script-response-body https://raw.githubusercontent.com/NanNan-Z/Rewrite/main/pornhub.js
-
 
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/NanNan-Z/Rewrite/main/pornhub.css" type="text/css">');
